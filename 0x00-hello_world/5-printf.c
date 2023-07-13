845c620ae79d011b70230c85b8ea227518fe1784
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-13 11:39:02 -0700

    18

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art, \n"); return 0; 
+#include <stdio.h>
+/**
+ * main - is a function that prints a text
+ *
+ * Return: 0 (success)
+ **/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}

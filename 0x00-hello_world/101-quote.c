b5625c1729af4552be028148664e08e2ba571cd0
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-13 12:17:44 -0700

    26

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is a function that prints a text
+ *
+ * Return: 1 (success)
+ **/
+int main(void)
+{
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19,");
+return (1);
+}

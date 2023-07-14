24809a8657b9c113f4a10483c11d9e6e15017bb5
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-14 08:15:56 -0700

    8

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
                         printf("Last digit of %d is %d and is greater than 5\n", n, d);
                 } else
                 {
-                        printf("Last digit of 629438752 is 2 and is less than 6 and not 0\n", n, d);
+                        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
                 }
         }
         return (0);

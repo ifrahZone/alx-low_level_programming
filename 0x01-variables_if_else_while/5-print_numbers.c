aba728bf0637ac8a03b3576d3584c1b993d6d02a
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-14 10:20:55 -0700

    19

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers.
+ *
+ * Return: 0 (success)
+ **/
+
+int main(void)
+{
+	int n;
+
+	for(n = 0;n < 10;n++)
+	{
+		printf(n);
+	}
+	printf("\n");
+	return (0);
+}

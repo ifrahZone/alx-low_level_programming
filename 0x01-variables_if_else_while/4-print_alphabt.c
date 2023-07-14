db10a26fb7b37f328a4b7d891e3ac755bca7cf0e
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-14 10:09:51 -0700

    17

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the alphabet in lowercase.
+ *
+ * Return: 0 (success)
+ **/
+
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z)
+	{
+		if ( alph != 'e' && alph != 'q')
+		{
+			putchar(alph);
+		}
+		alph++;
+	}
+
+	putchar('\n');
+	return (0);
+}

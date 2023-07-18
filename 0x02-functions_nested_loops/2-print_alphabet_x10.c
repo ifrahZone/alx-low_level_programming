781aff7a01ccdea9bd7b60d1397d2bb4c4719a7f
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-18 13:50:22 -0700

    12

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - a program that prints the alphabet in lowercase.
+ *
+ * Return: 0 (success)
+ **/
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	for(i = 1; i < 10; i++)
+	{
+		alph = 'a';
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+		_putchar('\n');
+	}
+}

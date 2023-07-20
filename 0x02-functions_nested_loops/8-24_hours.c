234c1808963e5941fca4dc803e67092bc4177362
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-20 12:29:22 -0700

    25

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,15 +3,15 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * 
+ *
  * Return: 0
- * */
+ */
 
 void jack_bauer(void)
 {
 	int min;
 	int hour;
-	
+
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (min = 0; min <= 59; min++)

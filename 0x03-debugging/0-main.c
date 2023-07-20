6d0ab151ccf346b3f2c92f5f18d0460e80593396
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-20 14:15:39 -0700

    34

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test
+ *
+ * Return : 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 3;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	printf("Infinite loop incoming :(\n");
+	i = 11;
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}

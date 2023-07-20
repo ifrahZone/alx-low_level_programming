91d6a1a2b107c4ee694d12b8643cc0e00a14ac7a
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-20 12:48:54 -0700

    29

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints natural from n to 98
+ * @n: number to start by
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+}

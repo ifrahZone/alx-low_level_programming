fcd1b5973d04172a08868b52aea57c715c344a95
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-20 11:57:32 -0700

    18

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdlib>
+#include "main.h"
+
+/**
+*_isalpha -  function that checks for alphabetic character.
+*@c: character
+*Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}

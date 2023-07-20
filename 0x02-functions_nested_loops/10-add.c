3e84466b63b75b6b6be38cdc709174a074a49b0a
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-20 12:41:03 -0700

    27

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - sums up two numbers
+ * @n:first number
+ * @m:second number
+ * Return: 0
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}

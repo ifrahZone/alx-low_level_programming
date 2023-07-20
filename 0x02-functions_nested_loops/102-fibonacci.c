256b585d43038172d75b40ff328c67d29fe34f18
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-20 13:38:43 -0700

    35

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long n1 = 0, n2 = 1, n3;
+	
+	for (i = 0; i < 50; i++)
+	{
+		n3 = n1 + n2;
+		printf("%lu", n3);
+		n1 = n2;
+		n2 = n3;
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}

400109c39252123b9533eb63ac59b7c25cbe9f58
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-24 09:23:56 -0700

    16

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - function that prints a string, in reverse
+ *
+ * @s: *char
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	printf("%s\n", s[-1]);
+}

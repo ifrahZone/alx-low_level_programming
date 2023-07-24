2856ad703e4f744f636c703768446e9d25dd2c69
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-24 16:34:23 -0700

    34

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ *
+ * @str: input
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int n = 0;
+	char *s = str;
+	int i;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	n = len - 1;
+	for (i = 0 ; i <= n ; i++)
+	{
+		if (i % 2 == 0)
+	{
+		_putchar(str[i]);
+	}
+	}
+	_putchar('\n');
+}
+

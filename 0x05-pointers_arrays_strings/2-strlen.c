1877fa94c289b35005c4806600b5cb99cccb5f15
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-24 08:56:55 -0700

    7

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: string
+ *
+ * Return: string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}

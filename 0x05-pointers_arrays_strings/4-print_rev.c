a26a528abd6a09bf16d2103a21fbe221a1d71b10
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-24 15:25:30 -0700

    18

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,17 +12,17 @@
 void print_rev(char *s)
 {
 	int len = 0;
-
-        while (*s != '\0')
-        {
-                len++;
-                s++;
-        }
-        s--;
-        while (*s > 0)
-        {
-                putchar(*s);
-                s--;
-        }
-		printf("\n");
+	
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	while (*s > 0)
+	{
+		putchar(*s);
+		s--;
+	}
+	printf("\n");
 }

eae3ea630820380dea48551380196956778b8f6a
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-07-24 09:14:08 -0700

    11

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function that prints a string, followed by a new line
+ *
+ * @str: char
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		printf("%p", *str);
+		str++;
+	}
+	printf("\n");
+}

43cdd00471fa45ccf495b7a6a800eb4ed716bfbd
ifrahZone <ifrah.fatimazahra10031999@gmail.com>
2023-08-14 04:34:38 -0700

    55

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,16 @@
 char *leet(char *n)
 {
 	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+	char str[] = "aAeEoOtTlL";
+	char code[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j])
+			if (n[i] == str[j])
 			{
-				n[i] = s2[j];
+				n[i] = code[j];
 			}
 		}
 	}
